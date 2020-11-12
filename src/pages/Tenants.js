2a0ff61c994a3f3c1be25dc89b8e15db3322d2d5
Lonwabo <lmsingelwa@gmail.com>
2020-11-12 15:32:36 +0200

    Edit Complaint

--- src/pages/Tenants.js
+++ src/pages/Tenants.js
@@ -1,54 +1,85 @@
-import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button, Menu, MenuItem } from '@material-ui/core';
+import { IoMdMenu } from 'react-icons/io';
+import { useState } from 'react';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
     },
-}));
+});
 
-export default function SelectedListItem() {
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Smith', "Waves", 6.0, 212121212, "lmsingelwa@gmail.com"),
+    createData('John', "Waves", 6.0, 212121212, "lmsingelwa@gmail.com"),
+    createData('Smith', "Waves", 6.0, 212121212, "lmsingelwa@gmail.com")
+];
+
+export default function Tenants() {
     const classes = useStyles();
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    const [show, setShow] = useState(false)
-
-    const showModal = () => {
-        setShow(true)
-    }
-
-    const hideModal = () => {
-        setShow(false)
-    }
-    const handleListItemClick = (event, index) => {
-        setSelectedIndex(index);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
-        <div className={classes.root}>
-            <List component="nav" aria-label="secondary mailbox folder">
-                <ListItem
-                    button
-                    selected={selectedIndex === 2}
-                    onClick={(event) => handleListItemClick(event, 2)}
-                >
-                    <ListItemText primary="Trash" />
-                </ListItem>
-                <ListItem
-                    button
-                    selected={selectedIndex === 3}
-                    onClick={(event) => handleListItemClick(event, 3)}
-                >
-                    <ListItemText primary="Spam" />
-                </ListItem>
-            </List>
-        </div>
+        <>
+            <h2 className="section-title p-3">Tenants</h2>
+            <TableContainer className="table-container" component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="center">Surname</TableCell>
+                            <TableCell align="center">Roon no</TableCell>
+                            <TableCell align="center">Contact no</TableCell>
+                            <TableCell align="center">Email</TableCell>
+                            <TableCell align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="center">{row.calories}</TableCell>
+                                <TableCell align="center">{row.fat}</TableCell>
+                                <TableCell align="center">{row.carbs}</TableCell>
+                                <TableCell align="center">{row.protein}</TableCell>
+                                <TableCell align="right"><Button aria-controls="simple-menu"
+                                    aria-haspopup="true" onClick={handleClick}>
+                                    <IoMdMenu></IoMdMenu></Button></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>View</MenuItem>
+                <MenuItem onClick={handleClose}>Delete</MenuItem>
+            </Menu>
+        </>
     );
 }
